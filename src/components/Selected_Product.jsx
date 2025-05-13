889d2c4e237745b5ebba9a3c8447b8d3ec36b00f
miri-bloy <m0504175155@gmail.com>
2025-05-14 00:06:47 +0300

    Empty components

---
+++ src/components/Selected_Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Selected_Product = () => {
+  return (
+    <div>Selected_Product</div>
+  )
+}
+
+export default Selected_Product
---
+++ src/components/Surplus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Surplus = () => {
+  return (
+    <div>Surplus</div>
+  )
+}
+
+export default Surplus
